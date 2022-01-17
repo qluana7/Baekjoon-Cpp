86ba15c7c6a07ebfa7b4f672b0d51fc171005f38
Lukince <whalstjd57@gmail.com>
2022-01-17 20:22:53 +0900

    11441

---
+++ Silver/Silver 3/11441.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int table[100'001];
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(false);
+    int n, m; cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> table[i]; table[i] += table[i - 1];
+    }
+    
+    cin >> m;
+    
+    for (int i = 0; i < m; i++) {
+        int a, b; cin >> a >> b;
+        
+        cout << table[b] - table[a - 1] << "\n";
+    }
+    
+    return 0;
+}
