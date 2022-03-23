91053f430dd7be4710ca72d26bb6f1366b586d4e
Lukince <emailluana7@gmail.com>
2022-03-23 21:18:52 +0900

    1927

---
+++ Silver/Silver 2/1927.cpp
@@ -0,0 +1,23 @@
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+    priority_queue<int, vector<int>, greater<int>> q;
+    int n; cin >> n;
+    
+    for (;n--;) {
+        int x; cin >> x;
+        if (x == 0) {
+            if (q.empty()) cout << "0\n";
+            else {
+                cout << q.top() << "\n";
+                q.pop();
+            }
+        } else q.push(x);
+    }
+    
+    return 0;
+}
