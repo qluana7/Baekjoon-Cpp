b022810560e54b391f5e61110712b61328054f02
Lukince <emailluana7@gmail.com>
2022-05-16 12:42:12 +0900

    1753

---
+++ Gold/Gold 5/1753.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define pii pair<int, int>
+#define INF 2147483647
+
+using namespace std;
+
+vector<vector<pii>> line_info;
+int max_node;
+
+struct cmp {
+    bool operator() (const pii& a, const pii& b) {
+        return a.second > b.second;
+    }
+};
+
+vector<int> dijkstra(int start) {
+    vector<int> v(max_node, INF);
+    
+    priority_queue<pii, vector<pii>, cmp> pq;
+    pq.emplace(start, 0);
+    v[start - 1] = 0;
+    
+    while (!pq.empty()) {
+        auto x = pq.top(); pq.pop();
+        
+        if (v[x.first - 1] < x.second) continue;
+        
+        for (const pii& p : line_info[x.first - 1]) {
+            int d = x.second + p.second;
+            if (d < v[p.first - 1]) {
+                v[p.first - 1] = d;
+                pq.emplace(p.first, d);
+            }
+        }
+    }
+    
+    
+    return v;
+}
+
+int main() {
+    int n, m, p; cin >> n >> m >> p; max_node = n;
+    
+    line_info = vector<vector<pii>>(n, vector<pii>());
+    
+    for (int i = 0; i < m; i++) {
+        int a, b, x; cin >> a >> b >> x;
+        line_info[a - 1].emplace_back(b, x);
+        // line_info[b - 1].emplace_back(a, x);
+    }
+    
+    auto res = dijkstra(p);
+    for (int i = 0; i < n; i++) {
+        cout << (res[i] == INF ? "INF" : to_string(res[i])) << "\n";
+    }
+    
+    return 0;
+}
