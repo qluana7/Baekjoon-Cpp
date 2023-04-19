36f85ad0f67a3c3f0625743ed3091c20cea5a15f
Luana7 <emailluana7@gmail.com>
2023-04-20 01:25:16 +0900

    2903

---
+++ Bronze/Bronze 3/2903.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr[] { 9, 25, 81, 289, 1089, 4225, 16641, 66049, 263169, 1050625, 4198401, 16785409, 67125249, 268468225, 1073807361 };
+    
+    int n; cin >> n; cout << arr[n - 1];
+}
