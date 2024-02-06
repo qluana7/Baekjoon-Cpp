0e31d99976a8408bae1ecf5a7217f6030b9fc994
Luana7 <emailluana7@gmail.com>
2024-02-06 18:49:58 +0900

    13028

---
+++ Platinum/Platinum 2/13028.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+struct Query {
+    int index, l, r;
+};
+
+int main() {
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+    
+    int n, m, k, mx; cin >> n >> m; k = sqrt(n);
+    vector<int> v(n);
+    for (auto& x : v) { cin >> x; mx = max(mx, x); }
+    
+    vector<Query> query(m);
+    for (int i = 0; i < m; i++) { cin >> query[i].l >> query[i].r; query[i].index = i; }
+    
+    sort(query.begin(), query.end(), [k](const Query& a, const Query& b) {
+        int l1 = a.l / k, l2 = b.l / k;
+        if (l1 != l2) return l1 < l2;
+        else return a.r < b.r;
+    });
+    
+    vector<int> output(m);
+    int l = query[0].l, r = query[0].r;
+    vector<int> b(mx, 0);
+    vector<int> bp(n, 0);
+    int sum = 0, bmx = 0;
+    for (int i = l; i <= r; i++) {
+        auto x = v[i - 1] - 1;
+        b[x]++;
+        bp[b[x] - 1]--; bp[b[x]]++;
+        
+        if (b[x] > bmx) bmx++;
+    }
+    for (int i = 3; i <= bmx; i++)
+        sum += bp[i];
+    
+    output[query[0].index] = sum;
+    
+    for (int i = 1; i < query.size(); i++) {
+        auto _l = query[i].l, _r = query[i].r;
+        
+        while (_l < l) { l--; auto x = v[l - 1] - 1; b[x]++; bp[b[x] - 1]--; bp[b[x]]++; if (b[x] == 3) sum++; if (b[x] > bmx) bmx++; }
+        while (_r > r) { r++; auto x = v[r - 1] - 1; b[x]++; bp[b[x] - 1]--; bp[b[x]]++; if (b[x] == 3) sum++; if (b[x] > bmx) bmx++; }
+        while (_l > l) { auto x = v[l - 1] - 1; b[x]--; l++; bp[b[x] + 1]--; bp[b[x]]++; if (b[x] == 2) sum--; if (b[x] + 1 == bmx && bp[b[x] + 1] == 0) bmx--; }
+        while (_r < r) { auto x = v[r - 1] - 1; b[x]--; r--; bp[b[x] + 1]--; bp[b[x]]++; if (b[x] == 2) sum--; if (b[x] + 1 == bmx && bp[b[x] + 1] == 0) bmx--; }
+        
+        output[query[i].index] = sum;
+    }
+    
+    for (auto x : output) cout << x << "\n";
+}
