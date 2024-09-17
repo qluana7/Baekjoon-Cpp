664976be88b62559638cf134fe3c40800bef72ac
Luana7 <53417057+qluana7@users.noreply.github.com>
2024-09-17 13:33:29 +0000

    Add ConvexHull

---
+++ Algorithm/ConvexHull.cpp
@@ -0,0 +1,102 @@
+/*
+ * Algorithm Name : Convex Hull
+ *
+ * Description
+ *    - 볼록 껍질 구현
+ *    - cvh_gs : 그레이엄 스캔 구현체
+ *    - cvh_mc : 모노톤 체인 구현체
+ * 
+ * Reference
+ *    - Graham Scan : ? (Reference missing)
+ *    - Monotone Chain : https://bamgoesn.github.io/posts/algorithms/2023-09-11-monotone-chain-cvh/
+ */
+
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+#define ll long long
+
+using namespace std;
+
+/* --- Common Implementation --- */
+struct pos_t {
+    pos_t() : pos_t(0, 0) { }
+    pos_t(int a, int b)
+        : x(a), y(b), p(1), q(0) { }
+    ll x, y, p, q;
+};
+
+inline ll ccw(pos_t s, pos_t a, pos_t b) {
+    return 1LL * (a.x - s.x) * (b.y - s.y) - 1LL * (b.x - s.x) * (a.y - s.y);
+}
+/* --- Common Implementation --- */
+
+bool cmp(const pos_t& p1, const pos_t& p2) {
+    if(1LL * p1.p * p2.q != 1LL * p2.p * p1.q)
+        return 1LL * p1.p * p2.q > 1LL * p2.p * p1.q;
+    if (p1.y != p2.y) return p1.y < p2.y;
+    return p1.x < p2.x;
+}
+
+stack<int> cvh_gs(vector<pos_t>& pt) {
+    sort(pt.begin(), pt.end(), cmp);
+
+    for (int i = 1; i < pt.size(); i++) {
+	    pt[i].p = pt[i].x - pt[0].x;
+	    pt[i].q = pt[i].y - pt[0].y;
+	}
+	
+	sort(pt.begin() + 1, pt.end(), cmp);
+	
+	stack<int> s;
+	
+	s.push(0);
+	s.push(1);
+	
+	for(int i = 2; i < pt.size(); i++) {
+	    while (s.size() >= 2) {
+	        int p2 = s.top(); s.pop();
+	        int p1 = s.top();
+	        
+	        if (ccw(pt[p1], pt[p2], pt[i]) > 0) {
+	            s.push(p2);
+	            break;
+	        }
+	    }
+	    
+	    s.push(i);
+	}
+
+    return s;
+}
+
+/* Usage : cvh_mc(v.begin(), v.end()); */
+template<typename ForwardIterator>
+vector<pos_t> cvh_mc(ForwardIterator _s, ForwardIterator _e) {
+    vector<pos_t> arr(_s, _e);
+
+    sort(arr.begin(), arr.end(), [](const pos_t& p1, const pos_t& p2) {
+        if (p1.x != p2.x) return p1.x < p2.x;
+        else return p1.y < p2.y;
+    });
+
+    vector<pos_t> top, bottom;
+    
+    for (auto b = arr.begin(), e = arr.end(); b != e; b++) {
+        while (top.size() >= 2 && ccw(top[top.size() - 2], top[top.size() - 1], *b) >= 0) top.pop_back();
+
+        top.push_back(*b);
+    }
+
+    for (auto b = arr.rbegin(), e = arr.rend(); b != e; b++) {
+        while (bottom.size() >= 2 && ccw(bottom[bottom.size() - 2], bottom[bottom.size() - 1], *b) >= 0) bottom.pop_back();
+
+        bottom.push_back(*b);
+    }
+
+    top.pop_back();
+    top.insert(top.end(), bottom.begin(), bottom.end() - 1);
+
+    return top;
+}
